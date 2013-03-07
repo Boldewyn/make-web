a70e5c912b8ba541dfebd420dce1233354bfd458
Manuel Strehl <m.strehl@kinetiqa.de>
2013-03-07 11:42:04 +0100

    make tidy more robust

--- tidy.mk
+++ tidy.mk
@@ -1,28 +1,43 @@
-TIDY_DIR := lib/tidy
-TIDY := $(TIDY_DIR)/bin/tidy
-TIDY_FLAGS := -e -q --drop-empty-elements no
+#
+# Get and build an HTML5 enabled tidy build
+#
+# This can be used to validate HTML files on build. Include
+# the `test-tidy` target and set the variable HTML_TARGETS
+# to list the HTML files to be tested.
+#
+
+TIDY_DIR ?= lib/tidy
+TIDY ?= $(TIDY_DIR)/bin/tidy
+TIDY_FLAGS ?= -e -q --drop-empty-elements no
+HTML_TARGETS ?=
 
 .PHONY: build-tidy clean-tidy test-tidy
 
 # build the tidy executable
 build-tidy: $(TIDY)
 
+ifdef TIDY
 $(TIDY):
-	git clone https://github.com/w3c/tidy-html5.git $(TIDY_DIR) && \
-		make -C $(TIDY_DIR)/build/gmake
+	$(info * build the tidy executable $(TIDY))
+	@if [ -d "$(TIDY_DIR)" ]; then \
+		git --git-dir="$(TIDY_DIR)/.git" --work-tree="$(TIDY_DIR)" pull; \
+	else \
+		git clone https://github.com/w3c/tidy-html5.git "$(TIDY_DIR)"; \
+	fi
+	@make -C "$(TIDY_DIR)/build/gmake"
+endif
 
 # remove tidy again
 clean-tidy:
-	rm -fr $(TIDY_DIR)
+	$(info * remove tidy)
+	@rm -fr "$(TIDY_DIR)"
 
 # test a bundle of HTML files with tidy
 test-tidy: build-tidy $(HTML_TARGETS)
-ifdef TIDY
 	$(info * test markup)
 	@for html in $(HTML_TARGETS); do \
 		echo "  > $$html"; \
-		$(TIDY) $(TIDY_FLAGS) $$html; \
+		"$(TIDY)" $(TIDY_FLAGS) $$html; \
 	done
-endif
 
 #END
