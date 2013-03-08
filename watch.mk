2a7700a52474080a23b31a1b73650fb749907575
Manuel Strehl <m.strehl@kinetiqa.de>
2013-03-08 10:04:47 +0100

    make watch more configurable

--- watch.mk
+++ watch.mk
@@ -1,20 +1,25 @@
 #
 # Watch directories for changes
 #
+# You should not do this, when you also use something like
+# compass watch or Python's watchdog in parallel.
+#
 
 WATCH_DIRS ?=
 WATCH_INIT ?=
 WATCH_TARGETS ?=
+WATCH_COMMAND ?= inotifywait
+WATCH_PARAMS ?= -q -q -e close_write -r $(WATCH_DIRS)
 
 .PHONY: watch
 
 watch:
 ifdef WATCH_INIT
 	@echo "* initial build...";
-	@make -s $(WATCH_TARGETS)
+	@$(MAKE) -s $(WATCH_TARGETS)
 endif
 	@while true; do \
-		inotifywait -q -q -e close_write -r $(WATCH_DIRS);\
+		$(WATCH_COMMAND) $(WATCH_PARAMS);\
 		echo "* change detected. Re-building...";\
-		make -s $(WATCH_TARGETS);\
+		$(MAKE) -s $(WATCH_TARGETS);\
 	done
