d4496e6bc01a90a042821a4e4ed97332536392d4
Manuel Strehl <git@manuel-strehl.de>
2013-03-08 22:41:47 +0100

    add composer support for PHP

--- php.mk
+++ php.mk
@@ -2,9 +2,16 @@
 # Tools to handle PHP
 #
 
+PHP_COMPOSER ?= bin/composer.phar
 PHP_SRC ?=
 
-.PHONY: test-php
+.PHONY: build-php deps-php test-php
+
+build-php:
+	curl -sS https://getcomposer.org/composer.phar >$(PHP_COMPOSER)
+
+deps-php: composer.json
+	php $(PHP_COMPOSER) install
 
 test-php: $(PHP_SRC)
 	! (for PHP in $(PHP_SRC); do \
