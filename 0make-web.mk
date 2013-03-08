58ca09d08027807b5fcd5247238a8d1fe2e4f8e8
Manuel Strehl <git@manuel-strehl.de>
2013-03-08 22:42:36 +0100

    add a placeholder Makefile
    We need it to cater for the
    include *.mk
    case before own rules

---
+++ 0make-web.mk
@@ -0,0 +1,9 @@
+.PHONY: make-web-dummy make-web
+
+make-web-dummy:
+	$(error If you see this message, it means most likely, that you have \
+	imported the make-web files before any of your own rules. Don't do that, \
+	see the README for details)
+
+make-web:
+	$(info See http://boldewyn.github.com/make-web/)
