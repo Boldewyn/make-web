b9d33cdb22e461a390fa3dead5242fd1dd0b7dd3
Manuel Strehl <git@manuel-strehl.de>
2013-03-09 22:18:11 +0100

    fix `find` command in images.mk

--- images.mk
+++ images.mk
@@ -5,7 +5,8 @@
 IMAGES_OPTIMIZE_PNG ?= optipng -o7
 IMAGES_OPTIMIZE_JPEG ?= jpegoptim --strip-all
 IMAGES_OPTIMIZE_GIF ?= gifsicle -i --batch
-IMAGES_SRC ?= $(shell find . -type f -name \*.png -name \*.gif -name \*.jpg)
+IMAGES_SRC ?= $(shell find . -type f \
+                \( -name \*.png -or -name \*.gif -or -name \*.jpg \) )
 
 .PHONY: images
 
@@ -19,5 +20,5 @@ else ifeq ($(suffix $<), .jpg)
 else ifeq ($(suffix $<), .gif)
 	$(IMAGES_OPTIMIZE_GIF) "$<"
 else
-	$(warning Unsupported suffix: $(suffix $<))
+	$(warning Unsupported file type: $(suffix $<))
 endif
